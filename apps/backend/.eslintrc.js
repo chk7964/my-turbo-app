fefa636a6a4526ee514c0e798b2bc8b6f71232a8
chk7964 <chk7964.developer@gmail.com>
2025-05-05 23:14:59 +0530

    vs

---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['../../.eslintrc.js'], // Inherit from root config
+  rules: {
+    // Add or override specific rules for the frontend here
+  },
+};
---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+
+  rules: {
+    // Custom ESLint rules here
+  },
+  ignorePatterns: ['node_modules', 'build', 'dist','.next','next.config.ts'], 
+};
